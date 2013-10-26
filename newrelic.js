4d834106871a2a45b2cbb48b7b17a3b577d75d1c
Andreas Lubbe <andreaslubbe@gmail.com>
2013-10-26 18:46:55 +0200

    added newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['TravelSquare'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : 'dd6f1cbaa5dc7bddfedf6db2bd21a0f7a8dcfc9b',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
