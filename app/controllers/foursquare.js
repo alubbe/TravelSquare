678d7fa8c4734de14464e6bf6e52d63479b53027
Toby <Toby@Toby>
2013-10-26 22:00:57 +0200

    Middle of long and lat

--- app/controllers/foursquare.js
+++ app/controllers/foursquare.js
@@ -11,3 +11,71 @@ exports.getFromFoursquare = function(req, res) {
 	
 	res.render('foursquare/test');
 };
+
+exports.getBerlin = function(req, res) {
+	var location = req.params.location;
+	//query API
+
+	var http = require('https');
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+	var options = {
+		host: 'https://api.foursquare.com',
+		path: '/v2/venues/search?client_id=WUH3Z4VTUYMQCD54KHR0O2BBXXSCIBIQ31I2NX2VGNL2T4AF&client_secret=S0LD0WYY11CYTJQZ01EYBBL0SGNSLUN0RXRXJOJMO0Y540WU&v=20130815%20%20%20&near='
+	};
+	options.path += location;
+
+	callback = function(response) {
+		var data = '';
+
+		response.on('data', function(chunk) {
+			data += chunk; //append data
+		});
+
+		response.on('end', function() {
+			console.log('finished receiving data');
+		});
+	};
+
+http.request(options, callback).end();
+res.render('foursquare/berlin', {
+		location: location
+	});
+};
+
+exports.getMiddleOfLongLat = function(coordArray) {
+	
+	//sums
+	var sumX = 0;
+	var sumY = 0;
+	var sumZ = 0;
+
+	//iteratre throught all coordinates
+	var coordCount = coordArray.length;
+	for (var i = 0; i >= coordCount; i++) {
+		//Convert into cartesian coordinates
+
+		//convert into radiants
+		var radiantLat = coordArray[i][0] * PI / 180;
+		var radiantLong = coordArray[i][1] * PI * 180;
+
+		var x = Math.cos(radiantLat) * Math.cos(radiantLong);
+		var y = Math.cos(radiantLat) * Math.sin(radiantLong);
+		var z = MAth.cos(radiantLat);
+
+		sumX += x;
+		sumY += y;
+		sumZ += z;
+	}
+
+	sumX /= coordCount;
+	sumY /= coordCount;
+	sumZ /= coordCount;
+
+	var resultLon = Math.atan2(sumY, sumX);
+    var resultHyp = Math.sqrt(sumX * sumX + sumY * sumY);
+    var resultLat = Math.atan2(sumZ, resultHyp);
+
+    return new Array(resultLon);
+
+};
