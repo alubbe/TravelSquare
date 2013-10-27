e35947672fb8b3302647a9f35fcc86d6ff952562
Toby <Toby@Toby>
2013-10-27 05:35:51 +0100

    deleted wrong middle of coord function

--- app/controllers/foursquare.js
+++ app/controllers/foursquare.js
@@ -5,52 +5,6 @@ var mongoose = require('mongoose'),
 	async = require('async'),
 	_ = require('underscore');
 
-exports.getMiddleOfLatLong = function(coordArray) {
-	
-	//sums
-	var sumX = 0;
-	var sumY = 0;
-	var sumZ = 0;
-	console.log(coordArray);
-
-	//iteratre throught all coordinates
-	var coordCount = coordArray.length;
-	for (var i = 0; i < coordCount; i++) {
-		//Convert into cartesian coordinates
-
-		//convert into radiants
-		var radiantLat = coordArray[i][0] *Math.PI / 180;
-		var radiantLong = coordArray[i][1] * Math.PI * 180;
-		
-
-		var x = Math.cos(radiantLat) * Math.cos(radiantLong);
-		var y = Math.cos(radiantLat) * Math.sin(radiantLong);
-		var z = Math.sin(radiantLat);
-		
-		console.log(y);
-
-		sumX += x;
-		sumY += y;
-		sumZ += z;
-	}
-
-	sumX /= coordCount;
-	sumY /= coordCount;
-	sumZ /= coordCount;
-
-	
-
-	var resultLon = Math.atan2(sumY, sumX);
-		var resultHyp = Math.sqrt(sumX * sumX + sumY * sumY);
-		var resultLat = Math.atan2(sumZ, resultHyp);
-
-		resultLon *= 180 / Math.PI;
-		resultLat *= 180 /Math.PI;
-
-		return [resultLat,resultLon];
-
-};
-
 exports.distanceBetween = function(lat1, lng1, lat2, lng2){
 	lat1 = lat1 * Math.PI / 180;
 	lat2 = lat2 * Math.PI / 180;
@@ -128,9 +82,9 @@ addMorePLaces = function(placesArray, category, radius, center, amount, callback
 	//add center
 	options.path += '&ll=' + center.lat + ',' + center.lon;
 	//add radius in meters
-	options.path += '&radius' + radius;
+	options.path += '&radius=' + radius;
 	//add category
-	options.path += '&query' + category;
+	options.path += '&query=' + category;
 
 	async = function(response) {
 		var data = '';
