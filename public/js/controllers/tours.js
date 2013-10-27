502a7541bd44ecfb6a93d0631d1c9b340efa0816
Sven Münnich <sven.muennich@viison.com>
2013-10-27 05:53:51 +0100

    Extend tours view

--- public/js/controllers/tours.js
+++ public/js/controllers/tours.js
@@ -28,11 +28,68 @@ angular.
 
             // Get data of current day
             var locations = [];
+            var currentSection;
+            $scope.sections = []
             for (var i in selectedDay.venues) {
+                // Save location
                 locations.push(selectedDay.venues[i].location);
+                // Add venue to a section
+                if (!currentSection || currentSection.code !== selectedDay.venues[i].timeCode) {
+                    // Create a new section
+                    currentSection = {
+                        code: selectedDay.venues[i].timeCode,
+                        selected: (i == 0),
+                        venues: []
+                    };
+                    $scope.sections.push(currentSection);
+                }
+                currentSection.venues.push(selectedDay.venues[i]);
             }
+
+            // Update the heights of all sections
+            for (var i in $scope.sections) {
+                $('#c'+i).css({
+                    height: $scope.getHeightOfSection(index)+'px'
+                });
+            }
+
+            // TODO: check why sectio is not opening
+            $scope.openSection(0);
 
             // Display the new route on the map
             mapsWrapper.updateRoute(locations);
         };
+
+        $scope.openSection = function(index) {
+            for (var i in $scope.sections) {
+                if (i == index) {
+                    // Display new section
+                    $('#c'+i).css({
+                        visibility: 'visible',
+                        display: 'block'
+                    }).animate({
+                        height: $scope.getHeightOfSection(index)+'px',
+                        padding: '5px'
+                    }, 300);
+                    continue;
+                }
+                // Hide all other sections
+                $scope.sections[i].selected = false;
+                $('#c'+i).animate({
+                    height: 0,
+                    padding: '0 5px'
+                }, 300, function () {
+                    $(this).css({
+                        visibility: 'hidden',
+                        display: 'none'
+                    });
+                });
+            }
+            $scope.sections[index].selected = true;
+        };
+
+        $scope.getHeightOfSection = function(index) {
+            return $scope.sections[index].venues.length * 23;
+        };
+
     }]);
