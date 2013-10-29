9b2fb64fec0c487efbd95f15ca219dda37bd5442
claudio_fianc <weck@fianc.de>
2013-10-29 11:46:00 +0100

    planning page now under plan.html, currently without angular

---
+++ public/exampledata/samplejson.js
@@ -0,0 +1,192 @@
+ var sampledata = {
+            venuesdata: [
+          
+     {
+
+		{"name": "Title of Place Food 1", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "6.24",
+			   "likes" :"36" ,
+			   "pictureUrl" : "food1.jpg",
+			    "fs" : {
+			    "id" : "1qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+         		{"name": "Title of Place Food 2", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "6.14",
+			   "likes" :"26" ,
+			   "pictureUrl" : "food2.jpg",
+			    "fs" : {
+			    "id" : "2qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 3", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "6.01",
+			   "likes" :"36" ,
+			   "pictureUrl" : "food3.jpg",
+			    "fs" : {
+			    "id" : "3qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 4", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "4.24",
+			   "likes" :"36" ,
+			   "pictureUrl" : "food4.jpg",
+			    "fs" : {
+			    "id" : "4qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 5", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "4.04",
+			   "likes" :"10" ,
+			   "pictureUrl" : "food5.jpg",
+			    "fs" : {
+			    "id" : "5qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 6", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "3.99",
+			   "likes" :"36" ,
+			   "pictureUrl" : "food6.jpg",
+			    "fs" : {
+			    "id" : "1qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 7", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "3.86",
+			   "likes" :"30" ,
+			   "pictureUrl" : "food7.jpg",
+			    "fs" : {
+			    "id" : "7qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 8", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "3.55",
+			   "likes" :"36" ,
+			   "pictureUrl" : "food8.jpg",
+			    "fs" : {
+			    "id" : "8qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 9", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "3.24",
+			   "likes" :"12" ,
+			   "pictureUrl" : "food9.jpg",
+			    "fs" : {
+			    "id" : "9qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 10", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "2.99",
+			   "likes" :"10" ,
+			   "pictureUrl" : "food10.jpg",
+			    "fs" : {
+			    "id" : "10qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+				{"name": "Title of Place Food 11", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "6.24",
+			   "likes" :"10" ,
+			   "pictureUrl" : "food11.jpg",
+			    "fs" : {
+			    "id" : "11qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		},
+
+
+
+		{"name": "Title of Place Food 12", 
+		"location" :
+			 {"address" : "",
+			  "lat" : "",
+			   "lng": ""
+			} , 
+			 "category" : "",
+			  "rating" : "2.24",
+			   "likes" :"8" ,
+			   "pictureUrl" : "food12.jpg",
+			    "fs" : {
+			    "id" : "1qwertyuio", 
+			    "url" : "https://foursquare.com/v/skihalle-neuss/4b80f131f964a520c19230e3"}
+		}
+
+            ]
+        };
+
+
+
