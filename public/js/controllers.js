8d01f815e8d7264d353c9b314151c5e47d39ba4a
Andreas Lubbe <andreaslubbe@gmail.com>
2013-10-30 16:14:25 +0100

    integrated planning html into app flow

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -29,9 +29,13 @@ travelSquareControllers.controller('PlanningCtrl', [
 	'$http',
 	'$location',
 	'$routeParams',
-	function ($scope, $rootScope, $http, $location, $routeParams) {
+	'$window',
+	'$timeout',
+	function ($scope, $rootScope, $http, $location, $routeParams, $window, $timeout) {
 		console.log('Planning controller loaded');
 		$scope.test = 'planning OK';
+		var $ = $window.jQuery;
+		console.log($);
 
 		// TODO
 		console.log('TODO: Load the planning data...')
@@ -39,6 +43,111 @@ travelSquareControllers.controller('PlanningCtrl', [
 		// Called when loading the index.html
 		$scope.setup = function() {
 			console.log('Planning view loaded');
+	    $scope.test = "Controller ok";
+	    // $scope.city =  $scope.$location.search().location ;
+	    // $scope.days =  $scope.$location.search().location ;   
+	    $scope.city = TSglobal_city;
+	    
+	    var urlparams, param_city = "Amsterdam", param_days = "2";
+	    if($window.location.href.split("?")[1] != null) {
+	    	urlparams = $window.location.href.split("?")[1].split("&");
+	    	param_city =  urlparams[0].split("=")[1];
+	    	param_days =  urlparams[1].split("=")[1];
+	    }
+      $scope.city = decodeURI(param_city);
+      $scope.days = decodeURI(param_days);
+
+	    $scope.sections = [];
+
+		  function TCget(turl, name) {
+		  	// console.log("TCGet: " + name);
+			  $http({method: 'GET', url: turl}).
+			  success(function(data, status) {
+				  // this callback will be called asynchronously
+				  // when the response is available
+				  $scope.sections[name] = data;
+			  }).
+			  error(function(data, status, headers, config) {
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			    console.log('TCget returned an error');
+			    console.log(data);
+			  });
+			};
+
+			function getUrl(section, amount) {
+			  return "http://localhost:3000/foursquare/" + $scope.city +  "/" + section + "/" + amount;
+			};
+
+			TCget(getUrl("restaurants"    ,6), "restaurants");
+			$timeout(function(){TCget(getUrl("coffee",6), "coffee"     )}, 300);
+			$timeout(function(){TCget(getUrl("sights",6), "sights"     )}, 600);
+			$timeout(function(){TCget(getUrl("arts"  ,6), "arts"       )}, 900);
+			$timeout(function(){TCget(getUrl("shops" ,6), "shops"      )}, 1200);
+			$timeout(function(){TCget(getUrl("drinks" ,6), "drinks"    )}, 1500);
+			$timeout(function(){TCget(getUrl("outdoors",6), "outdoors" )}, 1800);
+
+
+			TSglobal_likes = [];
+			TSglobal_dislikes = [];
+			TSglobal_visibles = [];
+			TSglobal_city = "Barcelona";
+			TSglobal_hotel = "JetPack Alternative";
+			TSglobal_startdate = "01.12.2013";
+			TSglobal_daysofstay = 5;
+			    
+			$( "#accordion" ).accordion({
+		      event: "click hoverintent",
+		      header: "h3" 
+		    });
+
+		  $( "input[type=submit]" )
+		      .button({
+		      icons: {
+		        primary: "ui-icon-locked"
+		      }
+		    })
+		      .click(function( event ) {
+		        event.preventDefault();
+		      }); 
+
+		  $( ".venueTrash" ).click(function() {
+		  	$(this).parents(".square").hide('fade');
+		    TSglobal_dislikes.push("idkommtnoch");
+		    console.log("dislike id 123");
+		    /*retrieve id, toggle on/off*/
+		  });
+
+		  $( ".venueHeart" ).click(function() {
+		  	$(this).removeClass( "imgoff" );
+		    TSglobal_likes.push("idkommtnoch");
+		    console.log("like id 123");
+		    /*retrieve id, toggle on/off*/
+		  });
+
+		  $("#calculatetrip").click(function(){
+		      prefix = "prefix";
+		      llikes = "";
+		      ldislikes = "";
+		      lothers = "";
+		      var nexturl = prefix + "&city=" + TSglobal_city + "&hotel=" + TSglobal_hotel + "&startdate=" + TSglobal_startdate + "&daysofstay=" + TSglobal_daysofstay + "&likes=" + TSglobal_likes + "&dislikes=" + TSglobal_dislikes + "&visibles=" + TSglobal_visibles;    
+		       $( "#debugnexturl" ).html(nexturl); 
+		       $( "#debugnexturl" ).dialog( "open" );
+
+      });
+
+	    $( "#debugnexturl" ).dialog({
+	      autoOpen: false,
+	      show: {
+	        effect: "blind",
+	        duration: 1000
+	      },
+	      hide: {
+	        effect: "explode",
+	        duration: 1000
+	      }
+	    });
+
 		}
 
 		// Called by the form in the planning.html
