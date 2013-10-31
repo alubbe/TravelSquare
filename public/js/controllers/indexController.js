3ca691cd1afa74b56cf6bb9b9be06df319fc0f37
Andreas Lubbe <andreaslubbe@gmail.com>
2013-10-31 21:23:05 +0100

    added code for google places autocomplete

--- public/js/controllers/indexController.js
+++ public/js/controllers/indexController.js
@@ -4,7 +4,8 @@ angular.module('travelSquare.index', []).controller('IndexCtrl', [
   '$http',
   '$location',
   '$routeParams',
-  function ($scope, $rootScope, $http, $location, $routeParams) {
+  '$window',
+  function ($scope, $rootScope, $http, $location, $routeParams, $window) {
     console.log('Index controller loaded');
 
     // Remove all URL parameters
@@ -15,14 +16,24 @@ angular.module('travelSquare.index', []).controller('IndexCtrl', [
     // Called when loading the index.html
     $scope.setup = function() {
       console.log('Index view loaded');
+      var maps = $window.google.maps;
+
+      //var autocompletemain = new maps.places.Autocomplete(document.getElementById('location'), {
+      //  types: ["(cities)"],
+      //  componentRestrictions: {country: 'de'}
+      //});
+
     }
 
     // Called by the form in the index.html
     $scope.submit = function(indexModel) {
-      // Update the URL
-      $location.path('/planning').
-        search('location', indexModel.location).
-        search('days', indexModel.days);
+      //TODO: add error handling if some input is missing
+
+      if(indexModel != null && indexModel.location != null && indexModel.days != null)
+        // Update the URL
+        $location.path('/planning').
+          search('location', indexModel.location).
+          search('days', indexModel.days);
     }
   }
 ]);
